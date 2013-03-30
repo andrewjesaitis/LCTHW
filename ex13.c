27ee7bf3d08be6a8c657152200ad5612fd6b0283
Andrew Jesaitis <andrew@andrewjesaitis.com>
2013-03-29 23:04:34 -0600

    Handles multiple words

--- ex13.c
+++ ex13.c
@@ -2,42 +2,46 @@
 
 int main(int argc, char *argv[])
 {
-	if(argc != 2) {
-		printf("ERROR: You need one argument.\n");
-		// this is how you abort a program
-		return 1;
-	}
+	// if(argc != 2) {
+	// 	printf("ERROR: You need one argument.\n");
+	// 	// this is how you abort a program
+	// 	return 1;
+	// }
+	int j = 0;
+	for(j = 1; j < argc; j++){
+		printf("Word %d:\n", j);
+		int i =  0;
+		for(i = 0; argv[j][i] != '\0'; i++) {
+			char letter = argv[j][i];
+			letter = letter | 32;
 
-	int i =  0;
-	for(i = 0; argv[1][i] != '\0'; i++) {
-		char letter = argv[1][i];
-		letter = letter | 32;
-
-		switch(letter) {
-			case 'a':
-				printf("%d: 'A'\n", i);
-				break;
-			case 'e':
-				printf("%d: 'E'\n", i);
-				break;
-			case 'i':
-				printf("%d: 'I'\n", i);
-				break;
-			case 'o':
-				printf("%d: 'O'\n", i);
-				break;
-			case 'u':
-				printf("%d: 'U'\n", i);
-				break;
-			case 'y':
-				if(i > 2) {
-					// it's only sometimes Y
-					printf("%d: 'Y'\n", i);
-				}
-				break;
-			default:
-				printf("%d: %c is not a vowel\n", i, letter);
+			switch(letter) {
+				case 'a':
+					printf("%d: 'A'\n", i);
+					break;
+				case 'e':
+					printf("%d: 'E'\n", i);
+					break;
+				case 'i':
+					printf("%d: 'I'\n", i);
+					break;
+				case 'o':
+					printf("%d: 'O'\n", i);
+					break;
+				case 'u':
+					printf("%d: 'U'\n", i);
+					break;
+				case 'y':
+					if(i > 2) {
+						// it's only sometimes Y
+						printf("%d: 'Y'\n", i);
+					}
+					break;
+				default:
+					printf("%d: %c is not a vowel\n", i, letter);
+			}
 		}
+
 	}
 
 	return 0;
