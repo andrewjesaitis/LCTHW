039f720027a3951d9535e733af1fb5ecfafe57d3
Andrew Jesaitis <andrew@andrewjesaitis.com>
2013-11-11 20:07:16 -0700

    Comments exercise 16

--- ex16.c
+++ ex16.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+//define our Person struct
 struct Person
 {
 	char *name;
@@ -11,29 +12,36 @@ struct Person
 	int weight;
 };
 
+//defines a function that creates a Person; similar to a constructor in OOP
 struct Person *Person_create(char *name, int age, int height, int weight)
 {
+	//allocate enough memory for a Person
 	struct Person *who = malloc(sizeof(struct Person));
 	assert(who != NULL);
 
+	//set the attributes of the new Person for which we just allocated memory
+	//first we use the strdup function to allocate memory and copy the name into that memory
 	who->name = strdup(name);
 	who->age = age;
 	who->height = height;
 	who->weight = weight;
 
+	//return a pointer to our person
 	return who;
 }
 
 void Person_destroy(struct Person *who)
 {
 	assert(who != NULL);
-
+	//free the memory that has been allocated to both the char array for the name
+	//and the Person struct itself
 	free(who->name);
 	free(who);
 }
 
 void Person_print(struct Person *who)
 {
+	//print out the attributes of our Person
 	printf("Name: %s\n", who->name);
 	printf("\tAge: %d\n", who->age);
 	printf("\tHeight: %d\n", who->height);
